974a1f9a679d81eeeaed014d5c64138d943042db
hieunc <hieunc@aoi-sys.vn>
2015-05-22 16:59:57 +0700

    Init Commit

---
+++ 01_front/protected/widgets/footerWidget.php
@@ -0,0 +1,13 @@
+<?php
+
+class footerWidget extends CWidget
+{
+    public function init()
+    {
+    }
+
+    public function run()
+    {
+        return $this->render('footer-widget');
+    }
+}
---
+++ 01_front/protected/widgets/headerWidget.php
@@ -0,0 +1,13 @@
+<?php
+
+class headerWidget extends CWidget
+{
+    public function init()
+    {
+    }
+
+    public function run()
+    {
+        return $this->render('header-widget');
+    }
+}
